bb7e2445cb4b4851e3c59e660de4ba88c0c0111a
seoane <seoane@zityhub.com>
2025-12-09 16:08:12 +0100

    TEC-628: add testing scripts

--- src/Logger.ts
+++ src/Logger.ts
@@ -5,17 +5,19 @@ type Message = string | Record<string, unknown>
 export default class Logger {
   private readonly logger: PinoLogger
 
-  constructor(name: string, sync = true) {
-    const env = process.env.NODE_ENV ?? 'development'
-    const isProd = env === 'production'
+  constructor(name: string, sync: boolean = true) {
+    this.logger = this.getLogger(name, sync)
+  }
 
-    this.logger = isProd
-      ? pino({ name, level: 'info' }, pino.destination({ minLength: 4096, sync }))
-      : pino({
-          name: `${name}-local`,
-          level: 'info',
-          transport: { target: 'pino-pretty', options: { colorize: true } }
-        })
+  private getLogger(name: string, sync: boolean) {
+    if (process.env.NODE_ENV === 'production') {
+      return pino({ name, level: 'info' }, pino.destination({ sync, ...(!sync && { minLength: 4096 }) }))
+    }
+    return pino({
+      name: `${name}-dev`,
+      level: 'debug',
+      transport: { target: 'pino-pretty', options: { colorize: true } }
+    })
   }
 
   debug(message: Message): void {
